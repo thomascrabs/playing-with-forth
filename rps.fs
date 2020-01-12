56a0cb345e58d6a4a09f76e88ca18b0a21d6ecec
thomas <thomasjcrabs@gmail.com>
2020-01-12 19:28:49 +0000

    Bit more messing with RPS

--- rps.fs
+++ rps.fs
@@ -1,11 +1,18 @@
-VARIABLE rnd  HERE rnd ! 
-: RANDOM  rnd @ 31421 *  6927 +  DUP rnd ! 
-: CHOOSE  ( u1 -- u2 )  RANDOM UM*  NIP 
 .( Welcome to Rock, Paper, Scissors, Forth style!) CR
 .( Please select your move [R|P|S]:) 
 KEY
 DUP
 CR
 .( You chose: ) EMIT CR
-3 CHOOSE
+.s CR
+VARIABLE rnd   HERE rnd !
+: RANDOM  rnd @ 31421 *  6927 +  DUP rnd ! ;
+: CHOOSE  ( u1 -- u2 )  RANDOM UM*  NIP ;
+\ something that produces something more random????? : REALCHOICE  ( n -- ) 0 DO 3 CHOOSE . LOOP ; 
+3 CHOOSE 
+DUP
+.s  CR
+.( Computer picked: ) EMIT CR
+\ : CHECKIFSAME ( -- ) = if ." Same value"
+CR
 bye
